1b69309796e207f55f9490eec95559521f1d4eb0
Rajat Kumar Saini <rajatsaini005@gmail.com>
2023-03-01 16:07:36 +0530

    Update index.js
    auth update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain="dev-e3auvekf0tr67sui.us.auth0.com"
-    clientId="pKyWFc1z8SN34kc31D3bxyJgRwQJaB3Q"
+    domain="dev-bfh78r60kbdu7nju.us.auth0.com"
+    clientId="ijWAk15PzFJ1537mb84qIhmlupaBy80j"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
